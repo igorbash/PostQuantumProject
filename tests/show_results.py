66aa53617ff4df699bc104e122d4470ca9bf88fb
igorbash <igor731996@gmail.com>
2024-06-28 16:29:05 +0300

    Fix script and add title

--- tests/show_results.py
+++ tests/show_results.py
@@ -12,17 +12,18 @@ def parse_data(output_file, table_results):
                 elif "Error" in results[i * 3 + j]:
                     table_results[i][j] = "Failure"
 
+
 table_results = matrix = [[0] * 3 for _ in range(6)]
 parsed_data = parse_data(OUTPUT_FILE, table_results)
 
 columns = ["Client", "KEM", "Classic Server", "Post Quantum Server", "Hybrid Server"]
 data = [
-    ["OpenSSL", "ECDH x25519", table_results[0][0], table_results[0][1], table_results[0][2]],
-    ["OpenSSL","Kyber768", table_results[1][0], table_results[1][1], table_results[1][2]],
-    ["OpenSSL", "Hybrid p521_kyber1024", table_results[2][0], table_results[2][1], table_results[2][2]],
-    ["BoringSSL", "ECDH x25519", table_results[3][0], table_results[3][1], table_results[3][2]],
-    ["BoringSSL", "Kyber768", table_results[4][0], table_results[4][1], table_results[4][2]],
-    ["BoringSSL", "Hybrid p521_kyber1024", table_results[5][0], table_results[5][1], table_results[5][2]],
+    ["OpenSSL", "ECDH x25519", table_results[0][0], table_results[1][0], table_results[2][0]],
+    ["OpenSSL","Kyber768", table_results[0][1], table_results[1][1], table_results[2][1]],
+    ["OpenSSL", "Hybrid p521_kyber1024", table_results[0][2], table_results[1][2], table_results[2][2]],
+    ["BoringSSL", "ECDH x25519", table_results[3][0], table_results[4][0], table_results[5][0]],
+    ["BoringSSL", "Kyber768", table_results[3][1], table_results[4][1], table_results[5][1]],
+    ["BoringSSL", "Hybrid p521_kyber1024", table_results[3][2], table_results[4][2], table_results[5][2]],
 ]
 fig, ax = plt.subplots(figsize=(8, 8))  # Adjust the size as needed
 ax.axis('off')
@@ -32,7 +33,10 @@ for i, column in enumerate(columns):
     table.add_cell(0, i, width=0.25, height=0.2, text=column, loc='center', facecolor='lightgrey', fontproperties={'weight': 'bold'})
 for row_index, row in enumerate(data):
     for col_index, cell in enumerate(row):
-        table.add_cell(row_index + 1, col_index, width=0.25, height=0.2, text=cell, loc='center')
+        if col_index == 0:
+            table.add_cell(row_index + 1, col_index, width=0.25, height=0.2, text=cell, loc='center',  fontproperties={'weight': 'bold'})
+        else:
+            table.add_cell(row_index + 1, col_index, width=0.25, height=0.2, text=cell, loc='center')
 for (i, j), cell in table.get_celld().items():
     if i > 0 and j > 0:
         if cell.get_text().get_text() == 'Success':
@@ -40,5 +44,6 @@ for (i, j), cell in table.get_celld().items():
         elif cell.get_text().get_text() == 'Failure':
             cell.set_facecolor('red')
 ax.add_table(table)
+plt.title("OpenSSL client and BoringSSL client connect to 3 servers with different certificate signature and different KEM algorithms")
 plt.tight_layout()
 plt.show()
