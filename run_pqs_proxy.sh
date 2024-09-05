34437291b7eff2e920b886b9b4a71270c122e669
igorbash <igor731996@gmail.com>
2024-09-05 21:06:36 +0300

    Fix proxy test

--- run_pqs_proxy.sh
+++ run_pqs_proxy.sh
@@ -4,10 +4,10 @@ cd pq_proxy
 
 sudo docker compose up -d 
 echo "Trying get site wtfismyip.com with hybrid KEM when the site dont support it"
-sudo docker compose run -it --rm client curl https://wtfismyip.com/json --curves x25519_kyber768
+sudo docker compose run -it --rm client curl https://wtfismyip.com/json --curves p521_kyber1024
 
 echo "Trying get site wtfismyip.com with hybrid KEM using the proxy - will succeed: use wireshark to capture the traffic"
 echo "Press Enter to continue..."
 read  
-sudo docker compose run -it --rm client curl -x https://pqProxy.com:4433 https://wtfismyip.com/json --curves x25519_kyber768
+sudo docker compose run -it --rm client curl -x https://pqProxy.com:4433 https://wtfismyip.com/json
 sudo docker compose down
